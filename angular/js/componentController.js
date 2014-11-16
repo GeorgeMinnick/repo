d015fc4111a7782a11132751069f001108a70783
BradCantor <bradleycantor@Brads-Air.home>
2014-11-16 16:38:37 -0500

    Tiny change

--- angular/js/componentController.js
+++ angular/js/componentController.js
@@ -6,5 +6,5 @@ function componentCtrl($scope){
    alert(num);
   }
        
-  $scope.text = "This is a built in Ember helper. It is created using the <textarea> tag but there are built-in directives associated with it. These include, ng-model,ng-required,ng-minlength,ng-maxlength,ng-pattern,ng-change, and ng-trim. It is also easy to bind this data to the model using the ng-model directive attribute.";
+  $scope.text = "This is a built in Angular helper. It is created using the <textarea> tag but there are built-in directives associated with it. These include, ng-model,ng-required,ng-minlength,ng-maxlength,ng-pattern,ng-change, and ng-trim. It is also easy to bind this data to the model using the ng-model directive attribute.";
 }
